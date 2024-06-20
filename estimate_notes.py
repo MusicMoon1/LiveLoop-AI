fd3ee834a148c1f06dbf05f88a87e4da280cb3a3
Robin Doerfler <doerflerrobin@gmail.com>
2024-06-20 16:10:11 +0200

    updated estimator to essentia

--- estimate_notes.py
+++ estimate_notes.py
@@ -1,12 +1,10 @@
 import librosa.display
 import matplotlib.pyplot as plt
 import numpy as np
-
+import essentia.standard as es
 
 from basic_pitch.inference import predict
 from basic_pitch import ICASSP_2022_MODEL_PATH
-
-
 
 
 def inputaudio(audiofile):
@@ -14,11 +12,9 @@ def inputaudio(audiofile):
     return samples, sr
 
 
-
 def cqt(samples, sr):
     cqt = librosa.cqt(y=samples, sr=sr, bins_per_octave=12)
     return cqt
-
 
 
 def plot(cqt):
@@ -32,7 +28,6 @@ def plot(cqt):
     plt.show()
 
 
-
 def active_notes(midi_data):
     """ Gets list of active notes from the midi piano roll. """
     piano_roll = midi_data.get_piano_roll()
@@ -42,7 +37,6 @@ def active_notes(midi_data):
     plt.ylabel('Pitch')
     plt.title('Piano Roll -- close this to proceed')
     plt.show()
-
     active_notes = []
     for row_idx in range(piano_roll.shape[0]):
         if np.any(piano_roll[row_idx] > 0):
@@ -50,20 +44,47 @@ def active_notes(midi_data):
     print(f"active notes in this loop: {active_notes}")
 
 
-if __name__ == "__main__":
-    audiofile = "61228__the-sacha-rush__piano1.wav"
-    # samples, sr = inputaudio(audiofile)
-    # cqt = cqt(samples, sr)
-    # plot(cqt)
+def transpose_notes(notes, octave=0):
+    if min(notes) >= 60:
+        return notes
+    notes = [int(note) + 12 for note in notes]
+    octave += 1
+    return transpose_notes(notes, octave)
 
-    # Predict Notes
-    model_output, midi_data, note_events = predict(audiofile)  # uses Spotify's Basic Pitch
+
+def estimate_pitch_basic(audio):
+    model_output, midi_data, note_events = predict(audio)
     # Get All Notes Played as Midi Notes
     all_notes = []
     for instrument in midi_data.instruments:
         for note in instrument.notes:
             all_notes.append(note.pitch)
+    return all_notes
+
+
+def estimate_pitch_melodia(audiofile):
+    # Load File
+    loader = es.EqloudLoader(filename=audiofile, sampleRate=44100)
+    audio = loader()
+
+    pitch_extractor = es.PredominantPitchMelodia(frameSize=2048, hopSize=128)
+    pitch_values, _ = pitch_extractor(audio)
+    # pitch_times = np.linspace(0.0, len(audio) / 44100.0, len(pitch_values))
+    onsets, durations, notes = es.PitchContourSegmentation(hopSize=128)(pitch_values, audio)
+
+    return notes
+
+
+if __name__ == "__main__":
+    # audiofile = "61228__the-sacha-rush__piano1.wav"
+    audiofile = "data/loops/Loop1_anahata.aif"
+    # Predict Notes
+
+    all_notes = set(estimate_pitch_melodia(audiofile))
+    # Transpose Notes so that Lowest Note is above 60
+    all_notes_transposed = transpose_notes(all_notes)
 
     print(f'All Present Notes: {all_notes}')
+
 
 
